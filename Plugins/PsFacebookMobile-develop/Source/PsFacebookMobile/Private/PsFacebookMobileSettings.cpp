dc8cb54098bbd0562c67c2f08bdb9bcb5e060a08
Viktor <zamiralov3ddd@mail.ru>
2021-08-25 09:38:13 +0300

    del plagin

--- Plugins/PsFacebookMobile-develop/Source/PsFacebookMobile/Private/PsFacebookMobileSettings.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2015-2021 Mail.Ru Group. All Rights Reserved.
-
-#include "PsFacebookMobileSettings.h"
-
-UPsFacebookMobileSettings::UPsFacebookMobileSettings(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
--- Plugins/PsFacebookMobile-develop/Source/PsFacebookMobile/Public/PsFacebookMobileAPI.h
+++
@@ -1,5 +0,0 @@
-// Copyright 2015-2021 Mail.Ru Group. All Rights Reserved.
-
-#pragma once
-
-#include "PsFacebookMobileLibrary.h"
