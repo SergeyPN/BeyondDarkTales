1d197357b3f52c56e3cf7e8f9e13f129d89ef956
SergeyPN <sergey.pop@gmail.com>
2021-10-28 21:29:13 +0900

    Delete Plugins/AndroidX-UE4-develop directory

--- Plugins/AndroidX-UE4-develop/README.md
+++
@@ -1,7 +0,0 @@
-# AndroidX UE4
-
-AndroidX support for Unreal Engine 4
-
-Built and tested for UE 4.26.
-
-Just add it to your project and have fun. Required for https://github.com/nprudnikov/PrFirebase
--- Plugins/AndroidX-UE4-develop/Source/AndroidX/Private/AndroidXSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Vladimir Alyamkin. All Rights Reserved.
-
-#include "AndroidXSettings.h"
-
-UAndroidXSettings::UAndroidXSettings()
-	: bAndroidXEnabled(true)
-	, bMultiDexEnabled(true)
-{
-}
