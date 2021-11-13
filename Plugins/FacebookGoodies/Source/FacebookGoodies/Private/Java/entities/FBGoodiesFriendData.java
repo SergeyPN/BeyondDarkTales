0c8af9525147a03ed134b918e0f373dc9c0bab66
Viktor <zamiralov3ddd@mail.ru>
2021-11-13 18:43:57 +0300

    123

--- Plugins/FacebookGoodies/Source/FacebookGoodies/Private/Java/entities/FBGoodiesFriendData.java
+++
@@ -1,8 +0,0 @@
-//// Copyright (c) 2020 Nineva Studios
-
-package com.ninevastudios.fbgoodies.entities;
-
-public class FBGoodiesFriendData {
-    public String name;
-    public String id;
-}
--- Plugins/FacebookGoodies/Source/FacebookGoodies/Private/Java/entities/FBGoodiesPaging.java
+++
@@ -1,9 +0,0 @@
-//// Copyright (c) 2020 Nineva Studios
-
-package com.ninevastudios.fbgoodies.entities;
-
-public class FBGoodiesPaging {
-    public FBGoodiesCursors cursors;
-    public String previous = "";
-    public String next = "";
-}
