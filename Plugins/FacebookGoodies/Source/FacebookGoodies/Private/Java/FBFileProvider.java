45d2fea325b76bb93706743db2324556f4dfcbb2
Viktor <zamiralov3ddd@mail.ru>
2021-11-13 18:32:11 +0300

    111

--- Plugins/FacebookGoodies/Source/FacebookGoodies/Private/Java/FBFileProvider.java
+++
@@ -1,9 +0,0 @@
-//// Copyright (c) 2020 Nineva Studios
-
-package com.ninevastudios.fbgoodies;
-
-import androidx.core.content.FileProvider;
-
-public class FBFileProvider extends FileProvider {
-
-}
--- Plugins/FacebookGoodies/Source/FacebookGoodies/Private/Java/entities/FBGoodiesCursors.java
+++
@@ -1,8 +0,0 @@
-//// Copyright (c) 2020 Nineva Studios
-
-package com.ninevastudios.fbgoodies.entities;
-
-public class FBGoodiesCursors {
-    public String before = "";
-    public String after = "";
-}
