91c98b6b05fd0cdd7c8cc4e0ee1c83d90827f6c1
SergeyPC <sergey.pop@gmail.com>
2022-06-18 02:21:59 +0900

    Update BuildLM.bat

--- BuildLM.bat
+++ BuildLM.bat
@@ -1,1 +1,1 @@
-"d:\EpicGames\UE_4.27\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "d:\EpicGames\Projects\BeyondDarkTales\BeyondDarkTales.uproject" -run=resavepackages -buildlighting -quality=Production -allowcommandletrendering -map=Caves_Main
+"e:\EpicGames\UE_4.27\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "e:\EpicGames\Projects\BeyondDarkTales\BeyondDarkTales.uproject" -run=resavepackages -buildlighting -quality=Production -allowcommandletrendering -map=Caves_Main
